6c4b3edf444b6425b1b588d921f3955fe828b23f
LorenzaBriz0t0 <lbrizoto22@usp.br>
2024-07-28 22:27:17 -0300

    first commit

---
+++ src/Anotacoes.java
@@ -0,0 +1,42 @@
+// Fundamentos de Programação Orientada a Objetos com Java
+
+// Conceitos Básicos:
+// Linguagens de baixo-nível:
+// Mais próximas do hardware e bytecodes
+// Linguagens de alto-nível: 
+// Sintaxe e melhor compreensão do desenvolvedor
+// Programação estruturada:
+// Paradigma de programação com estruturas sequenciais ou procedimentos lineares
+// Programação orientada a objetos:
+// Dados -> Campos ou atributos
+// Códigos -> Procedimentos ou métodos
+// Representação do mundo real com objetos -> Abstração do mundo real
+// Programação estruturada:
+// Voltada à procedimentos e funções definidas pelo usuário
+// Programação orientada a objetos:
+// Voltada à classes e objetos
+
+// Classes
+// Todo aquivo Java é denominado classe -> Compilação de .java para .class
+// Identificador, estados(atributos ou propriedades), comportamentos e instância
+// Criação de objetos que representam uma classe e suas características
+
+// Classe de modelo (model) -> estrutura de domínio -> Cliente, Pedido
+// Classe de serviço (service) -> Regras e validação
+// Classe de repositório (repository) -> Integração com banco de dados
+// Classe de controle (controller) -> Comunicação externa à aplicação, http web ou webservices
+// Classe utilitária (util) -> Recursos comuns à aplicações
+
+// Pacotes
+// Organização de classes -> Subdiretórios do src
+// Convenções de pacotes
+// Ver exemplo em "com.mastersoft.clinical"
+
+
+
+
+
+
+
+
+
